710eaf3134cc4f20552e87389ae53880af672a42
antoine <adelacoux@infotrafic.com>
2021-06-07 12:14:18 +0200

    fix @DI

--- Code/test.py
+++ Code/test.py
@@ -52,7 +52,7 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
 
     rawCapture.truncate(0)
     t2 = time.perf_counter()
-    fichier.write(t2+" "+t2-t1)
+    fichier.write(str(t2)+" "+str(t2-t1))
     print(t2-t1)
 
 fichier.close()
