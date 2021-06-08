d894227860babb32795317088d43c57c154dd03c
antoine <adelacoux@infotrafic.com>
2021-06-08 18:26:32 +0200

    test

--- head-pose-estimation/estimate_head_pose.py
+++ head-pose-estimation/estimate_head_pose.py
@@ -142,10 +142,10 @@ def main():
             # pose_estimator.draw_annotation_box(frame, pose[0], pose[1], color=(255, 128, 128))
 
             # Uncomment following line to draw stabile pose annotation on frame.
-            # pose_estimator.draw_annotation_box(frame, steady_pose[0], steady_pose[1], color=(128, 255, 128))
+            pose_estimator.draw_annotation_box(frame, steady_pose[0], steady_pose[1], color=(128, 255, 128))
 
             # Uncomment following line to draw head axes on frame.
-            pose_estimator.draw_axes(frame, steady_pose[0], steady_pose[1])
+            # pose_estimator.draw_axes(frame, steady_pose[0], steady_pose[1])
 
         # Show preview.
         cv2.imshow("Preview", frame)
