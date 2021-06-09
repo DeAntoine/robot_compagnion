b52212e836f652918b280ca2d69c75178cf41238
antoine <adelacoux@infotrafic.com>
2021-06-09 15:27:00 +0200

    test

--- Code/test.py
+++ Code/test.py
@@ -82,13 +82,13 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
 
         if ang > 50 :
 
-            ls.write(to_bytes("g"))
+            ls.to_bytes("g")
             print("g")
             #serialArduino.write(to_bytes("g"))
 
         elif ang < 40 :
 
-            ls.write(to_bytes("d"))
+            ls.to_bytes("d")
             print("d")
             #serialArduino.write(to_bytes("d"))
 
