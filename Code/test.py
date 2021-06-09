d9cd09e3f9eb12833b5cab0bd27d870d8646c40c
antoine <adelacoux@infotrafic.com>
2021-06-09 15:35:56 +0200

    test

--- Code/test.py
+++ Code/test.py
@@ -27,7 +27,7 @@ t0 = time.perf_counter()
 #serialArduino = serial.Serial('/dev/ttyACM0', 9600)
 ang = 30
 print("test")
-ls.write(serial.to_bytes('d'))
+ls.write('d')
 print("fok")
 for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
