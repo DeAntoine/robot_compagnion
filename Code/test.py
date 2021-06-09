15779a61142e0c864f2f856c43bcb353e2cd25b6
antoine <adelacoux@infotrafic.com>
2021-06-09 18:07:20 +0200

    test

--- Code/test.py
+++ Code/test.py
@@ -46,6 +46,8 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
 
     cv2.waitKey(5)
 
+    #detect faces
+    faces = getFaces(frame)
 
 
     if i != 0 :
@@ -54,8 +56,6 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
         fichier.write(str(t2-t0)+" 0\n")
         fichier.write(str(t2-t0)+" 3\n")
 
-    #detect faces
-    faces = getFaces(frame)
 
     if len(faces) == 0 :
 
