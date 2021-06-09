bd9a3e448ae1773f1c729f141e954f783497bdf2
antoine <adelacoux@infotrafic.com>
2021-06-09 10:23:30 +0200

    test

--- Code/servo2.py
+++ Code/servo2.py
@@ -17,28 +17,30 @@ def angle_to_percent (angle) :
 
     return start + angle_as_percent
 
+def set_servo_angle(id,angle) :
+    GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
+    GPIO.setwarnings(False) #Disable warnings
 
-GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
-GPIO.setwarnings(False) #Disable warnings
+    #Use pin 12 for PWM signal
+    pwm_gpio = id
+    frequence = 50
+    GPIO.setup(pwm_gpio, GPIO.OUT)
+    pwm = GPIO.PWM(pwm_gpio, frequence)
 
-#Use pin 12 for PWM signal
-pwm_gpio = 12
-frequence = 50
-GPIO.setup(pwm_gpio, GPIO.OUT)
-pwm = GPIO.PWM(pwm_gpio, frequence)
+    #Init at 0°
+    pwm.start(angle_to_percent(90+angle))
+    time.sleep(1)
 
-#Init at 0°
-pwm.start(angle_to_percent(90))
-time.sleep(1)
+    #Go at 90°
+    pwm.ChangeDutyCycle(angle_to_percent(90+angle))
+    time.sleep(1)
 
-#Go at 90°
-pwm.ChangeDutyCycle(angle_to_percent(90))
-time.sleep(1)
+    #Finish at 180°
+    pwm.ChangeDutyCycle(angle_to_percent(90+angle))
+    time.sleep(1)
 
-#Finish at 180°
-pwm.ChangeDutyCycle(angle_to_percent(90))
-time.sleep(1)
+    #Close GPIO & cleanup
+    pwm.stop()
+    GPIO.cleanup()
 
-#Close GPIO & cleanup
-pwm.stop()
-GPIO.cleanup()
+set_servo_angle(12,20)
