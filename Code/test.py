dea59074e472e677b4c02fd965b99e9cbe6f00da
antoine <adelacoux@infotrafic.com>
2021-06-09 18:26:12 +0200

    test

--- Code/test.py
+++ Code/test.py
@@ -28,17 +28,17 @@ serialArduino = serial.Serial('/dev/ttyACM0', 9600)
 ang = 30
 
 serialArduino.write(b'g')
-
+compte=1
 
 for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
     time_start = time.perf_counter()
-    t1 = time.perf_counter()
+    time = time.perf_counter()
     if i != 0 :
-        fichier.write(str(t1-t0)+" 1\n")
-        fichier.write(str(t1-t0)+" 0\n")
-        fichier.write(str(t1-t0)+" 2\n")
-
+        fichier.write(str(time-t0)+" "+str(compte)+"1\n")
+        fichier.write(str(time-t0)+" 0\n")
+        fichier.write(str(time-t0)+" "+str(compte+1)+"1\n")
+        compte=compte+1
 
     frame = frame1.array
 
@@ -46,20 +46,14 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
 
     cv2.waitKey(5)
 
-    t2 = time.perf_counter()
-    if i != 0 :
-        fichier.write(str(t2-t0)+" 2\n")
-        fichier.write(str(t2-t0)+" 0\n")
-        fichier.write(str(t2-t0)+" 3\n")
-
     #detect faces
     faces = getFaces(frame)
 
-    t3 = time.perf_counter()
     if i != 0 :
-        fichier.write(str(t3-t0)+" 3\n")
-        fichier.write(str(t3-t0)+" 0\n")
-        fichier.write(str(t3-t0)+" 4\n")
+        fichier.write(str(time-t0)+" "+str(compte)+"1\n")
+        fichier.write(str(time-t0)+" 0\n")
+        fichier.write(str(time-t0)+" "+str(compte+1)+"1\n")
+        compte=compte+1
 
 
     if len(faces) == 0 :
@@ -68,7 +62,6 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
 
         # Give dir for a human
         dir_people = dp.detect(frame)
-        t4 = time.perf_counter()
 
         if dir_people != "r":
 
@@ -93,11 +86,12 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
         # detect head pose
         # ang = hpe.estimate_pose(frame)
 
-        t4 = time.perf_counter()
         if i != 0 :
-            fichier.write(str(t4-t0)+" 4\n")
-            fichier.write(str(t4-t0)+" 0\n")
-            fichier.write(str(t4-t0)+" 5\n")
+            fichier.write(str(time-t0)+" "+str(compte)+"1\n")
+            fichier.write(str(time-t0)+" 0\n")
+            fichier.write(str(time-t0)+" "+str(compte+1)+"1\n")
+            compte=compte+1
+
 
         if(ang == 60):
             ang = 30
@@ -124,20 +118,19 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
     time_end = time.perf_counter()
     print(time_end-time_start)
     rawCapture.truncate(0)
-    t4 = time.perf_counter()
     i=i+1
-    if i != 1 :
-        if len(faces) == 0 :
-            fichier.write(str(t4-t0)+" 4\n")
-        else :
-            fichier.write(str(t4-t0)+" 5\n")
-    else :
-        t0 = time.perf_counter()
 
-    fichier.write(str(t4-t0)+" 0\n")
-    if i == 2 :
-        break
-    fichier.write(str(t4-t0)+" 1\n")
+
+    if i != 0 :
+        time = time.perf_counter()
+        fichier.write(str(time-t0)+" "+str(compte)+"1\n")
+        fichier.write(str(time-t0)+" 0\n")
+        if i == 2 :
+            break
+        compte=1
+        fichier.write(str(t4-t0)+" 1\n")
+
+
 
 # Arduino Esclave
 
