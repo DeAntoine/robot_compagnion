0d4c58f7112ed036c37a477aac078960137b9433
HugoD98 <56720344+HugoD98@users.noreply.github.com>
2021-06-10 10:52:05 +0200

    Update estimate_head_pose.py

--- Code/estimate_head_pose.py
+++ Code/estimate_head_pose.py
@@ -194,6 +194,133 @@ def main():
     pwm_12.stop()
     pwm_32.stop()
     GPIO.cleanup()
+    
+ def estimate_direction(camera):
+        
+    # Introduce mark_detector to detect landmarks.
+    mark_detector = MarkDetector()
+    
+    rawCapture = PiRGBArray(camera, size = (320,240))
+    frame = 0
+    
+    for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        frame = frame1.array
+        break
+         
+    
+    # Setup process and queues for multiprocessing.
+    img_queue = Queue()
+    box_queue = Queue()
+    img_queue.put(frame)
+    box_process = Process(target=get_face, args=(
+        mark_detector, img_queue, box_queue,))
+    box_process.start()
 
-if __name__ == '__main__':
-    main()
+    # Introduce pose estimator to solve pose. Get one frame to setup the
+    # estimator according to the image size.
+    height, width = frame.shape[:2]
+    pose_estimator = PoseEstimator(img_size=(height, width))
+
+    # Introduce scalar stabilizers for pose.
+    pose_stabilizers = [Stabilizer(
+        state_num=2,
+        measure_num=1,
+        cov_process=0.1,
+        cov_measure=0.1) for _ in range(6)]
+
+    tm = cv2.TickMeter()
+
+        
+    rawCapture = PiRGBArray(camera, size = (320,240))
+
+    GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
+    #GPIO.setwarnings(False) #Disable warnings
+
+    #Use pin id for PWM signal
+    frequence = 50
+    GPIO.setup(12, GPIO.OUT)
+    pwm_12 = GPIO.PWM(12, frequence)
+    pwm_12.start(0)
+
+    GPIO.setup(32, GPIO.OUT)
+    pwm_32 = GPIO.PWM(32, frequence)
+    pwm_32.start(0)
+
+    GPIO.setup(15, GPIO.OUT)
+    GPIO.output(15, GPIO.HIGH)
+    print("pouette")
+
+
+#while camera.isOpened():
+    for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+        pwm_12.ChangeDutyCycle(0)
+        pwm_32.ChangeDutyCycle(0)
+
+        frame = frame1.array
+       
+        # Feed frame to image queue.
+        img_queue.put(frame)
+
+        # Get face from box queue.
+        facebox = box_queue.get()
+
+        if facebox is not None:
+            # Detect landmarks from image of 128x128.
+            face_img = frame[facebox[1]: facebox[3],
+                             facebox[0]: facebox[2]]
+            face_img = cv2.resize(face_img, (CNN_INPUT_SIZE, CNN_INPUT_SIZE))
+            face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2RGB)
+
+            tm.start()
+            marks = mark_detector.detect_marks(face_img)
+            tm.stop()
+
+            # Convert the marks locations from local CNN to global image.
+            marks *= (facebox[2] - facebox[0])
+            marks[:, 0] += facebox[0]
+            marks[:, 1] += facebox[1]
+
+            # Uncomment following line to show raw marks.
+            #mark_detector.draw_marks(frame, marks, color=(0, 255, 0))
+
+            # Uncomment following line to show facebox.
+            #mark_detector.draw_box(frame, [facebox])
+
+            # Try pose estimation with 68 points.
+            pose = pose_estimator.solve_pose_by_68_points(marks)
+
+            # Stabilize the pose.
+            steady_pose = []
+            pose_np = np.array(pose).flatten()
+            for value, ps_stb in zip(pose_np, pose_stabilizers):
+                ps_stb.update([value])
+                steady_pose.append(ps_stb.state[0])
+            steady_pose = np.reshape(steady_pose, (-1, 3))
+            #print(steady_pose)
+            # Uncomment following line to draw pose annotation on frame.
+            # pose_estimator.draw_annotation_box(frame, pose[0], pose[1], color=(255, 128, 128))
+
+            # Uncomment following line to draw stabile pose annotation on frame.
+            pose_estimator.draw_annotation_box(frame, steady_pose[0], steady_pose[1], pwm_12,pwm_32, color=(128, 255, 128))
+
+            # Uncomment following line to draw head axes on frame.
+            # pose_estimator.draw_axes(frame, steady_pose[0], steady_pose[1])
+        else:
+          break
+        
+        # Show preview.
+        cv2.imshow("Preview", frame)
+        if cv2.waitKey(10) == 27:
+            break
+            
+        rawCapture.truncate(0)
+        
+
+    # Clean up the multiprocessing process.
+    box_process.terminate()
+    box_process.join()
+    pwm_12.stop()
+    pwm_32.stop()
+    GPIO.cleanup()
+
