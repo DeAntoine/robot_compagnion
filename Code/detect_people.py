91d54231442e855d5e61201483a3140594700e1f
HugoD98 <56720344+HugoD98@users.noreply.github.com>
2021-06-14 14:39:50 +0200

    Update detect_people.py

--- Code/detect_people.py
+++ Code/detect_people.py
@@ -13,7 +13,7 @@ def detect(frame):
 
     frame = cv2.resize(frame, (640, 480))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8))
+    boxes, weights = hog.detectMultiScale(gray, winStride=(8,8))
     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
     
     if np.size(weights) > 0:
