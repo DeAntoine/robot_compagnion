8f65e005a8305acfeedb9906478fdb3283725be4
HugoD98 <56720344+HugoD98@users.noreply.github.com>
2021-06-15 15:51:27 +0200

    Update pose_estimator.py

--- Code/pose_estimator.py
+++ Code/pose_estimator.py
@@ -190,7 +190,7 @@ class PoseEstimator:
         #print(dir_12)
         #print(dir_32)
         pwm_12.ChangeDutyCycle(self.angle_to_percent(dir_12))
-        pwm_32.ChangeDutyCycle(self.angle_to_percent(dir_32))
+        pwm_32.ChangeDutyCycle(self.angle_to_percent(dir_12))
 
         """
         # Draw all the lines
