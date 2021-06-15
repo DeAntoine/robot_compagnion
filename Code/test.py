d042a228d398c0f3238ea576220adf88b0c5df57
HugoD98 <56720344+HugoD98@users.noreply.github.com>
2021-06-15 13:03:02 +0200

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -151,10 +151,10 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port
         print("\t\tlargeur tete : ",largeur_tete)
         print("\t\tlongueur tete : ",longueur_tete)
         
-        if longueur_tete < 45 :
+        if longueur_tete < 40 :
             ls.write('a')
             
-        elif longueur_tete > 55 :
+        elif longueur_tete > 60 :
             ls.write('r')
         
         elif xMil < (width/2)-30 :
