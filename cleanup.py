6407f6ccfd018cd697457d7ffd2388b72baaf8ba
antoine <adelacoux@infotrafic.com>
2021-06-16 13:22:05 +0200

    test @ad

--- cleanup.py
+++ cleanup.py
@@ -4,13 +4,14 @@ import RPi.GPIO as GPIO
 
 # when your code ends, the last line before the program exits would be...
 
+GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
 
-
-GPIO.cleanup()
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(32, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
+
+GPIO.cleanup()
 
 # remember, a program doesn't necessarily exit at the last line!
