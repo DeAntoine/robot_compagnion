2032b66e1b0895fe5d6ba4e0fe6301eedaac50ef
antoine <adelacoux@infotrafic.com>
2021-06-16 13:20:56 +0200

    test @ad

--- cleanup.py
+++ cleanup.py
@@ -3,6 +3,14 @@ import RPi.GPIO as GPIO
 # the rest of your code would go here
 
 # when your code ends, the last line before the program exits would be...
-GPIO.cleanup()
 
-# remember, a program doesn't necessarily exit at the last line! 
+
+
+GPIO.cleanup()
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(32, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+
+# remember, a program doesn't necessarily exit at the last line!
